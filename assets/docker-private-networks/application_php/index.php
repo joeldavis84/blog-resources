020564f055db1ab60b34a2403b0eee5932c59e2e
Joel Davis <joel.davis@duke.edu>
2017-07-25 13:04:16 -0400

    finished connection back to the database

--- assets/docker-private-networks/application_php/index.php
+++ assets/docker-private-networks/application_php/index.php
@@ -1,3 +1,19 @@
-<?php  
-echo "hello world!";  
-?>
+<html>
+  <head>
+    <title>Docker Private Network Example</title>
+  </head>
+  <body>
+
+    <?php
+
+      $dbh = new mysqli("172.18.0.22", "root", "bollocks", "testing123");
+      $result = $dbh->query("SELECT * FROM messages");
+
+      while ($row = $result->fetch_assoc())
+        echo $row['message_text'] ."<br/>";
+
+    ?>
+
+  </body>
+</html>
+
